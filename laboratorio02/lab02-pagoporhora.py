82ff7da7ae74c91d6a3c18a2f2822615536961a7
AndresMelara <jose.melara@est.zamorano.edu>
2022-09-11 16:59:50 -0600

    ejercicio8.2 lab02

---
+++ laboratorio02/lab02-pagoporhora.py
@@ -0,0 +1,6 @@
+# Programa que calcula el valor de pago por horas trabajadas
+hora = input("Dijite su cantidad de horas trabajadas: ")
+pago = input ("Dijite el pago por cada hora trabajada en USD $ ")
+
+total = int(hora) * int(pago)
+print ("Su pago por hora es: ", total)
