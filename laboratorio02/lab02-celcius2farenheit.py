dd5665dd2b78cad74ebf782ed8a25646f0e44354
AndresMelara <jose.melara@est.zamorano.edu>
2022-09-11 17:07:40 -0600

    ejercicio8.3 lab02

---
+++ laboratorio02/lab02-celcius2farenheit.py
@@ -0,0 +1,4 @@
+# Programa que calcula el valor de grados celcius a Farenhei
+celcius = float(input ("Digite su temperatura en celcius "))
+farenheit = (9/5) * celcius + 32
+print("Su temperatura en Farenheit es: ", farenheit)
