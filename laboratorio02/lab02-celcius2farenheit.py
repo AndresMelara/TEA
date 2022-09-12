dff2cc2dac980267b9110608da048da4fac42722
AndresMelara <jose.melara@est.zamorano.edu>
2022-09-12 17:12:25 -0600

    prueba 2

--- laboratorio02/lab02-celcius2farenheit.py
+++ laboratorio02/lab02-celcius2farenheit.py
@@ -3,3 +3,4 @@ celcius = float(input ("Digite su temperatura en celcius "))
 farenheit = (9/5) * celcius + 32
 print("Su temperatura en Farenheit es: ", farenheit)
 #prueba de push
+#prueba 2 de push
